ab899890a83849c84b48d18913b5c4faf7230dbc
Anuradha Gunasekara <theanuradha@gmail.com>
2017-01-23 21:26:31 +0530

    fix to ej-bizibo

--- ej-bizibo/src/org/entirej/ejinvoice/database/TEMOSJbossConnectionFactory.java
+++ ej-bizibo/src/org/entirej/ejinvoice/database/TEMOSJbossConnectionFactory.java
@@ -43,7 +43,7 @@ public class TEMOSJbossConnectionFactory implements EJConnectionFactory
                 assert dataSourceID != null;
                 Context initContext = new InitialContext();
                 
-                dataSource = (DataSource)initContext.lookup("java:/ej-invoice");
+                dataSource = (DataSource)initContext.lookup("java:/ej-bizibo");
                 
                 
                 
